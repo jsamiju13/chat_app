b180178dedec14782a00506b6cf6d449e404af0e
DaizoZTX <daitoztx@gmail.com>
2025-08-01 09:26:36 -0400

    x

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chat_app/theme.dart';
 import 'package:chat_app/splash_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+// new merge
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
